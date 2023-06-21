b83f02d9df16ef93bacb1c72266ac14eaf3a2a06
Vandeson Santos <vandeson.eng.quimica@gmail.com>
2023-06-21 02:48:22 -0300

    events, forks, watchers e estilização

--- src/js/objects/screen.js
+++ src/js/objects/screen.js
@@ -6,24 +6,35 @@ const screen = {
                                         <div class="data">
                                             <h1>${user.name ?? "Não possui nome cadastrado 😢"}</h1>
                                             <p>${user.bio ?? "Não possui bio cadastrado 😢"}</p>
-                                            <span>👥 ${user.followers} Seguidores ♾️ ${user.following} Seguindo </span>
+                                            <p>👥 ${user.followers} Seguidores</p>
+                                            <p>♾️ ${user.following} Seguindo</p>
                                         </div>
                                      </div>` 
         
         let repositoriesItens = ""
-        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
+        user.repositories.forEach(repo => repositoriesItens += `<li>
+                                                                    <a href="${repo.html_url}" target="_blank">
+                                                                    ${repo.name}
+                                                                        <span>
+                                                                            <p>🔋${repo.forks}</p>
+                                                                            <p>⭐${repo.stargazers_count}</p>
+                                                                            <p>👀${repo.watchers}</p>
+                                                                            <p>💻${repo.language}</p>
+                                                                        </span>
+                                                                    </a>
+                                                                </li>`)
         
         if (user.repositories.length > 0){
-            this.userProfile.innerHTML += `<div class="repositories section">
+            this.userProfile.innerHTML += `<div class="repositories  section">
                                                 <h2>Repositórios</h2>
                                                 <ul>${repositoriesItens}</ul>
                                             </div>`
         } 
         
         let eventsRepositories = ""
-        user.events.forEach(event => eventsRepositories += `<li>${event.repo.name}</li>`)
+        user.events.forEach(event => eventsRepositories += `<li><span>${event.repo.name}</span> - ${event.type}</li>`)
 
-        this.userProfile.innerHTML += `<div>
+        this.userProfile.innerHTML += `<div class="events">
                                             <h2>Events</h2>
                                             <ul>
                                                 ${eventsRepositories}
